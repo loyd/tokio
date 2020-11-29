58bccca399c7a46d1b72395a8ce9845b8309c50d
github-action-benchmark <github@users.noreply.github.com>
2020-11-29 16:41:10 +0000

    add rt_multi_threaded (cargo) benchmark result for c55d846f4b248b4a72335d6c57829fa6396ab9a5

---
+++ dev/bench/data.js
@@ -0,0 +1,56 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1606668070651,
+  "repoUrl": "https://github.com/loyd/tokio",
+  "entries": {
+    "rt_multi_threaded": [
+      {
+        "commit": {
+          "author": {
+            "email": "alice@ryhl.io",
+            "name": "Alice Ryhl",
+            "username": "Darksonn"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "c55d846f4b248b4a72335d6c57829fa6396ab9a5",
+          "message": "util: add rt to tokio-util full feature (#3194)",
+          "timestamp": "2020-11-29T09:48:31+01:00",
+          "tree_id": "5f27b29cd1018796f0713d6e87e4823920ba5084",
+          "url": "https://github.com/loyd/tokio/commit/c55d846f4b248b4a72335d6c57829fa6396ab9a5"
+        },
+        "date": 1606668067890,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "chained_spawn",
+            "value": 199336,
+            "range": "± 28519",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "ping_pong",
+            "value": 733466,
+            "range": "± 100952",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "spawn_many",
+            "value": 5426271,
+            "range": "± 672184",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "yield_many",
+            "value": 20837047,
+            "range": "± 2407771",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ]
+  }
+}
